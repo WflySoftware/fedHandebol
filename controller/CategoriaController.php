9be318868fd4295a2b2cc75b54eab1745d9c668c
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2013-10-17 19:04:51 -0300

    Arquitetura MVC PHP

---
+++ controller/CategoriaController.php
@@ -0,0 +1,32 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'].'/mds2013/persistence/CategoriaDAO.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/mds2013/model/Categoria.php');
+class CategoriaController{
+	private $categoriaDAO;
+	
+	public function __construct(){
+		$this->categoriaDAO = new CategoriaDAO();
+	}
+	public function _listarTodas(){
+		return $this->categoriaDAO->listarTodas();
+	}
+	public function _listarTodasAlfabicamente(){
+		return $this->categoriaDAO->listarTodasAlfabicamente();
+	}
+	public function _consultarPorId($id){
+		return $this->categoriaDAO->consultarPorId($id);
+	}
+	public function _consultarPorNome($nomeCategoria){
+		return $this->categoriaDAO->consultarPorNome($nomeCategoria);
+	}
+	public function _inserirCategoria(Categoria $categoria){
+		return $this->categoriaDAO->inserirCategoria($categoria);
+	}
+	public function _inserirCategoriaArrayParse($arrayCategoria){
+		$dadosCategoria = new Categoria();
+		for($i=0; $i<count($arrayCategoria);$i++){
+			$dadosCategoria->__setNomeCategoria($arrayCategoria[$i]);
+			$this->categoriaDAO->inserirCategoria($dadosCategoria);
+		}
+	}
+}
