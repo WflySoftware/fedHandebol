240aa0b6819500ec98411e93d0c3444c92ded06c
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2013-10-19 13:36:58 -0300

    Implementacao das classes ArbitroDAO e JogadorDAO

--- persistence/ArbitroDAO.php
+++ persistence/ArbitroDAO.php
@@ -1,1 +1,51 @@
 <?php
+include_once (__APP_PATH.'/model/Arbitro.php');
+include_once (__APP_PATH.'/persistence/Conexao.php');
+
+class ArbitroDAO{
+	private $conexao;
+	
+	public function __construct(){
+		$this->conexao = new Conexao();
+	}
+	
+	public function listarTodos(){
+		$sql = "SELECT * FROM arbitro";
+		$resultado = $this->conexao->banco->Execute($sql);
+		while($registro = $resultado->FetchNextObject()){
+			$dadosArbitro = new Arbitro();
+			$dadosArbitro->__constructOverload($registro->ID_ARBITRO,$registro->NOME,$registro->CPF);
+			$retornaArbitros[] = $dadosArbitro;
+		}
+		return $retornaArbitros;
+	}
+	
+	public function inserir(Arbitro $dadosArbitro){
+		$sql = "INSERT INTO arbitro (nome,telefone,cpf) VALUES ('{$dadosArbitro->__getNome()}','{$dadosArbitro->__getTelefone()}','{$dadosArbitro->__getCpf()}')";
+		$this->conexao->banco->Execute($sql);
+	}
+	
+	public function atualizar(Arbitro $dadosArbitro){
+		$sql = "UPDATE arbitro SET nome='{$dadosArbitro->__getNome()}', telefone='{$dadosArbitro->__getTelefone()}', cpf='{$dadosArbitro->__getCpf()}' WHERE id_arbitro='{$dadosArbitro->__getIdArbitro()}' ";
+		$this->conexao->banco->Execute($sql);
+		return $dadosArbitro;
+	}
+	
+	public function consultarPorId($id){
+		$sql = "SELECT * FROM arbitro WHERE id_arbitro= '{$id}'";
+		$resultado = $this->conexao->banco->Execute($sql);
+		$registro = $resultado->FetchNextObject();
+		$dadosArbitro = new Arbitro();
+		$dadosArbitro->__constructOverload($registro->ID_ARBITRO,$registro->NOME,$registro->CPF);
+		return $dadosArbitro;
+	}
+	
+	public function consultarPorNome($nome){
+		$sql = "SELECT * FROM arbitro WHERE nome= '{$nome}'";
+		$resultado = $this->conexao->banco->Execute($sql);
+		$registro = $resultado->FetchNextObject();
+		$dadosArbitro = new Arbitro();
+		$dadosArbitro->__constructOverload($registro->ID_ARBITRO,$registro->NOME,$registro->CPF);
+		return $dadosArbitro;
+	}
+}
--- persistence/JogadorDAO.php
+++ persistence/JogadorDAO.php
@@ -1,1 +1,60 @@
 <?php
+include_once (__APP_PATH.'/model/Jogador.php');
+include_once (__APP_PATH.'/model/Time.php');
+include_once (__APP_PATH.'/persistence/Conexao.php');
+
+class JogadorDAO{
+	private $conexao;
+	
+	public function __construct(){
+		$this->conexao = new Conexao();
+	}
+	
+	public function listarTodos(){
+		$sql = "SELECT * FROM jogador";
+		$resultado = $this->conexao->banco->Execute($sql);
+		while($registro = $resultado->FetchNextObject()){
+			$dadosJogador = new Jogador();
+			$dadosJogador->__constructOverload($registro->ID_JOGADOR,$registro->TIME_ID_TIME,$registro->NOME,$registro->DATA_NASCIMENTO,$registro->CPF,$registro->NUMERO);
+			$retornaJogadors[] = $dadosJogador;
+		}
+		return $retornaJogadors;
+	}
+	
+	public function inserir(Jogador $dadosJogador){
+		$sql = "INSERT INTO jogador (time_id_time,nome,data_nascimento,cpf,numero) VALUES ('{$dadosJogador->__getIdTime()}','{$dadosJogador->__getDataNascimento()}','{$dadosJogador->__getCpf()}','{$dadosJogador->__getNumero()}')";
+		$this->conexao->banco->Execute($sql);
+	}
+	
+	public function atualizar(Jogador $dadosJogador){
+		$sql = "UPDATE jogador SET time_id_time='{$dadosJogador->__getIdTime()}',nome='{$dadosJogador->__getNome()}', data_nascimento='{$dadosJogador->__getDataNascimento()}', cpf='{$dadosJogador->__getCpf()}', numero='{$dadosJogador->__getNumero()}' WHERE id_jogador='{$dadosJogador->__getIdJogador()}' ";
+		$this->conexao->banco->Execute($sql);
+		return $dadosJogador;
+	}
+	
+	public function consultarPorId($id){
+		$sql = "SELECT * FROM jogador WHERE id_jogador= '{$id}'";
+		$resultado = $this->conexao->banco->Execute($sql);
+		$registro = $resultado->FetchNextObject();
+		$dadosJogador = new Jogador();
+		$dadosJogador->__constructOverload($registro->ID_JOGADOR,$registro->TIME_ID_TIME,$registro->NOME,$registro->DATA_NASCIMENTO,$registro->CPF,$registro->NUMERO);
+		return $dadosJogador;
+	}
+	
+	public function consultarPorNome($nome){
+		$sql = "SELECT * FROM jogador WHERE nome= '{$nome}'";
+		$resultado = $this->conexao->banco->Execute($sql);
+		$registro = $resultado->FetchNextObject();
+		$dadosJogador = new Jogador();
+		$dadosJogador->__constructOverload($registro->ID_JOGADOR,$registro->TIME_ID_TIME,$registro->NOME,$registro->DATA_NASCIMENTO,$registro->CPF,$registro->NUMERO);
+		return $dadosJogador;
+	}
+	public function consultarPorTime(Time $dadosTime){
+		$sql = "SELECT * FROM jogador WHERE time_id_time= '{$dadosTime->__getIdTime()}'";
+		$resultado = $this->conexao->banco->Execute($sql);
+		$registro = $resultado->FetchNextObject();
+		$dadosJogador = new Jogador();
+		$dadosJogador->__constructOverload($registro->ID_JOGADOR,$registro->TIME_ID_TIME,$registro->NOME,$registro->DATA_NASCIMENTO,$registro->CPF,$registro->NUMERO);
+		return $dadosJogador;
+	}
+}
