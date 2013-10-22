d421f393685e7f747efd46ad5f42ce449826b136
Lucas Andrade <passaroalado@hotmail.com>
2013-10-22 15:04:47 -0200

    criando o metodo _listarTimeParaTabela() para a classe TimeController

--- controller/TimeController.php
+++ controller/TimeController.php
@@ -17,6 +17,31 @@ class TimeController{
 		}
 		return $arraySelect;
 	}
+	public function _listarTimeParaTabela(){
+		$dadosTime = new Time();
+		$arrayDadosTime = $this->timeDAO->listarTodos();
+		for($i=0;$i<count($arrayDadosTime); $i++){
+			$dadosTime = $arrayDadosTime[$i];
+			$arrayTr[] = "
+			<tr>
+          			<td><input type=\"checkbox\" value=\"1\" name=\"marcar[]\" /></td>
+					
+					<td>".$dadosTime->__getIdTime()."</td>
+          			<td>".$dadosTime->__getidTecnico()."</td>
+					<td>".$dadosTime->__getCategoria()."</td>
+          			<td>".$dadosTime->__getEndereco()."</td>
+          			<td>".$dadosTime->__getDataFundacao()."</td>
+          			<td>".$dadosTime->__getPresidente()."</td>
+					<td>".$dadosTime->__getTelefone()."</td>
+          			
+          			<td>
+            			<a href=\"#\"><img src=\"./views/images/edit.png\" width=\"16\" height=\"16\" /></a>
+            			<a href=\"#\"><img src=\"./views/images/delete.png\" width=\"16\" height=\"16\" /></a>
+          			</td>
+			</tr>";
+		}
+		return $arrayTr;
+	}
 	public function _listarTodos(){
 		return $this->timeDAO->listarTodos();
 	}
