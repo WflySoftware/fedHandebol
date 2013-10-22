359f24479e6ceb0d9b688908029eba3b860c92d2
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2013-10-22 17:13:10 -0200

    ajustes de identacao e conserto do bd

--- views/import.php
+++ views/import.php
@@ -1,8 +1,6 @@
-﻿		<script type="text/javascript" src="./views/assets/puc1imu.js"></script>
-		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-		<script type="text/javascript" src="./views/assets/Chart.js"></script>
+﻿
 		<script type="text/javascript" src="./views/assets/jquery.min.js"></script>
-		<script type="text/javascript" src="./views/assets/modernizr.js"></script>
+		<!--  <script type="text/javascript" src="./views/assets/modernizr.js"></script> -->
         <script type="text/javascript" src="./views/js/mascaras.js"></script>
         <script src="./views/js/jquery.tablesorter.min.js"></script>
     	<script src="./views/js/jquery.tablesorter.pager.js"></script>
