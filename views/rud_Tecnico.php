ea9d5dda8164198c2832c01202a53d35d0ce88c6
Guilherme Junio <guilhermeruj@gmail.com>
2013-10-22 17:24:13 -0200

    alinhamento da rud_tecnico

--- views/rud_Tecnico.php
+++ views/rud_Tecnico.php
@@ -2,14 +2,116 @@
 require_once __APP_PATH.'/view/TecnicoView.php';
 $tecnicoVW = new TecnicoView();
 ?>
-    <form method="post" action="exemplo.html" id="frm-filtro">
+<style>
+*, html{
+	font-family: Helvetica, Arial,sans-serif;
+	margin: 0px;
+	padding: 0px;
+	font-size: 12px;
+}
+
+body{
+	margin: 10px;
+}
+
+h1{
+	font-size:40px;
+	font-family: Georgia, "Times New Roman", serif;
+	letter-spacing:-2px;
+}
+
+a img{
+	border:none;
+}
+
+form{
+  background-color:#f2f2f2;
+  width:600px;
+  margin:10px 0 10px 0;
+  text-align:center;
+}
+
+form p{
+  padding:12px;
+}
+
+form p label{
+  color:#333;
+  font-weight:900;
+}
+
+form input{
+  padding:6px;
+  border:1px solid #ccc;
+  width:300px;
+}
+
+table{
+  width:600px;
+  border:1px solid #ccc;
+}
+
+table thead th{
+  background-color:#ad2525;
+  padding:6px;
+  color:#fff;
+  text-align:center;
+  font-size:14px;
+}
+
+table thead th.header{
+  cursor:pointer;
+}
+
+table tbody td{
+  padding:6px;
+  text-align:center;
+  color:#333;
+}
+
+table tbody tr.odd td{
+  background-color:#ffffcc;
+}
+
+table tbody tr.hover td{
+  background-color:#a9d0f5;
+}
+
+table tbody tr.selected td{
+  background-color:#a9f5a9!important;
+}
+
+.pagedisplay{
+	width:40px;
+	text-align:center;
+	border:none;
+	background-color:#f2f2f2;
+	font-weight:900;
+}
+
+#pager span{
+	font-weight:900;
+	display:inline-block;
+	margin:0 0 0 20px;
+	color:#666;
+	float:right;
+}
+
+#pager form{
+	text-align:left;
+	padding:10px;
+	width:580px;
+}
+</style>
+
+    <form style="margin: 0 auto" method="post" action="exemplo.html" id="frm-filtro">
       <p>
         <label for="pesquisar">Pesquisar</label>
         <input type="text" id="pesquisar" name="pesquisar" size="30" />
       </p>
     </form>
     
-    <table  cellspacing="0" summary="Tecnicos">
+    <table style="margin: 0 auto"  cellspacing="0" summary="Tecnicos">
       <thead>
         <tr>
           <th><input type="checkbox" value="1" id="marcar-todos" name="marcar-todos" /></th>
@@ -31,7 +133,7 @@ $tecnicoVW = new TecnicoView();
     </table>
     
     <div id="pager" class="pager">
-    	<form>
+    	<form style="margin: 0 auto">
 				<span>
 					Exibir <select class="pagesize">
 							<option selected="selected"  value="1">1</option>
