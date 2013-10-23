8078a657a93d112cf55701c9874cadd3e4d09e68
Guilherme Junio <guilhermeruj@gmail.com>
2013-10-23 11:36:45 -0200

    index

--- views/initial.php
+++ views/initial.php
@@ -1,18 +1,60 @@
 <?php ?>
-		<section id="features">
-		<article id="feature1">
-		<img src="./views/assets/RA1.png">
-		<h3>Regiões Administrativas</h3>
-		<p>Saiba os índices de ocorrência em sua Região Administrativa com um simples filtro e consiga medir os níveis de criminalidade.</p>
-		</article>
-		<article id="feature2">
-		<img src="./views/assets/circleChart.png">
-		<h3>Fácil de Entender e Ilustrado</h3>
-		<p>Todos os indices são bem facéis de entender e vem ilustrados em forma de gráfico para melhor visualização.</p>
-		</article>
-		<article id="feature3">
-		<img src="./views/assets/circle.png">
-		<h3>Filtre as Informações</h3>
-		<p>Disponíveis filtros para facilitar mais a pesquisa e exibir somente as informações que o usuário desejar.</p>
-		</article>
-		</section>
+<style>
+#caixa1{
+	margin: 0 auto;
+	width: 1024px;
+}
+#graficos{
+	width: 660px;
+	margin: 0 auto;
+}
+table{
+	margin: 0 auto;
+	border: 1px solid #CCC;
+	border-radius: 3px 3px 3px 3px; 
+}
+tr{
+	border: 1px solid #ccc;
+}
+th{
+	border: 1px solid #ccc;
+}
+</style>
+        <div id="caixa1">
+        <h2 style="margin: 0 auto; width: 1024px; text-align:center;">Garficos</h2><br/>
+         	<div id="graficos">
+            	<div id="butao-cliente">
+                	12<br />Time
+                </div>
+                <div id="butao-cliente">
+                	100<br />Jogadores
+                </div>
+                <div id="butao-cliente">
+                	10<br />tecnico
+                </div>
+                <div id="butao-cliente">
+                	5<br />Arbritos
+                </div>
+                <div id="clear"></div>            
+             </div>
+        </div>
+        <table border=0 style="width: 600px;">
+        	<thead>
+            	<tr style="background: #09F; border:#09f; color: #fff;">
+                	<th>Time</th>
+                    <th>V</th>
+                    <th>D</th>
+                    <th>G.F</th>
+                    <th>G.L</th>
+                </tr>
+            </thead>
+            	<tr>
+                	<th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>4</th>
+                    <th>5</th>
+                </tr>
+        </table>
+                   
+            	
