a57a1596e146e849aa3b63a23622ca62eff6582a
Lucas Andrade <passaroalado@hotmail.com>
2013-11-03 20:40:29 -0200

    modificando visualmente e colocando caixas de formulario na sumula

--- views/sumula.php
+++ views/sumula.php
@@ -49,6 +49,19 @@ td{
     -webkit-border-radius: 4px;
     border-radius: 4px;
 }
+.inputCaixa{
+    font-family: Arial, Verdana;
+    font-size: 20px;
+    padding: 5px;
+    border: 1px solid #b9bdc1;
+    width: 50px;
+    height: 40px;
+    color: #797979;
+	margin-bottom: 10px;
+	   -moz-border-radius: 4px;
+    -webkit-border-radius: 4px;
+    border-radius: 4px;
+}
 .but {
 display: inline-block;
 background-color: #ccc;
@@ -91,6 +104,10 @@ background-color: #CC0000;
 	margin-left: 5px;
 }
 </style>
+<?php 
+require_once __APP_PATH.'/view/JogadorView.php';
+$jogadorVW = new JogadorView();	
+?>
 <h2 style="margin: 0 auto; width: 1024px; text-align:center;">INICIAR JOGO  No.:</h2>
 <form method="post" style="margin: 0 auto;" action="">
 <table class="medidas" border="0" style="border-bottom: 1px solid #CCC; padding: 4px;">
@@ -104,11 +121,31 @@ background-color: #CC0000;
 </tr>
 <tr>
 	<td>A</td>
-	<td class="fundo medida-td2"></td>
+	<td>
+		<select class="input" name="idTime" id="idTime" required>
+	        	<?php 
+	        		$options = $jogadorVW->listarTimesParaSelect();
+	        		for($i=0;$i<count($options);$i++){
+						echo $options[$i];
+					}
+	        	?>
+	    </select>
+    </td>
     <td><small>Versos</small></td>
 	<td>B</td>
-    <td class="fundo medida-td2"></td>
-	<td class="fundo medida-td1"></td>
+   <td>
+		<select class="input" name="idTime" id="idTime" required>
+	        	<?php 
+	        		$options = $jogadorVW->listarTimesParaSelect();
+	        		for($i=0;$i<count($options);$i++){
+						echo $options[$i];
+					}
+	        	?>
+	    </select>
+    </td>
+	<td>
+		<input type="text" class="inputCaixa" name="espectadores" id="espectadorez" placeholder="Nº" style="text-align: center;" required>
+    </td>
 </tr>
 
 </table>
@@ -122,7 +159,16 @@ background-color: #CC0000;
     <td class="medida-td2"><small>Duração</small></td>
 </tr>
 <tr>
-	<td class="medida-td2"><select class="input"><option>Cidade1</option><option>Cidade2</option><option>Cidade3</option></select></td>
+	<td class="medida-td2">
+		<select class="input" name="idTime" id="idTime" required>
+        	<?php 
+        		$options = $jogadorVW->listarTimesParaSelect();
+        		for($i=0;$i<count($options);$i++){
+					echo $options[$i];
+				}
+        	?>
+	    </select>
+	</td>
     <td class="medida-td2"><select class="input"><option>local1</option><option>local2</option><option>local2</option></select></td>
     <td class="medida-td1"><input class="input2" type="date" name="data" id="data"></td>
     <td class="medida-td1"><input class="input2" type="time" name="hora" id="hora"></td>
@@ -138,56 +184,109 @@ background-color: #CC0000;
 
 <table class="medidas" border="0" style=" border-bottom: 1px solid #CCC; padding: 4px; margin-bottom: 10px;">
 	<tr>
-    	<td></td>
+    	
         <td><small>A</small></td>
+       	<td></td>
         <td><small>B</small></td>
         <td></td>
         <td><small>A</small></td>
-    	<td><small>B</small></td>
         <td></td>
+    	<td><small>B</small></td>
+    	<td></td>
         <td><small>A</small></td>
+        <td></td>
         <td><small>B</small></td>
         <td></td>
     	<td><small>A</small></td>
+    	<td></td>
         <td><small>B</small></td>
         <td></td>
         <td><small>A</small></td>
+        <td></td>
         <td><small>B</small></td>
+        <td></td>
     </tr>
     <tr>
-    	<td>1° Tempo(30)'</td>
-        <td class="fundo"></td>
-        <td class="fundo"></td>
-        <td>Fim do Jogo(60)'</td>
-        <td class="fundo"></td>
-        <td class="fundo"></td>
-        <td>1° Tempo Extra</td>
-        <td class="fundo"></td>
-        <td class="fundo"></td>
-        <td>2° Tempo Extra</td>
-    	<td class="fundo"></td>
-        <td class="fundo"></td>
-        <td>Tiro de 7 Metros</td>
-        <td class="fundo"></td>
-        <td class="fundo"></td>
+    	
+        <td>
+        	<input type="text" class="inputCaixa" name="placarA" id="placarA" placeholder="Nº" style="text-align: center;"  required>
+        </td>
+        <td>X</td>
+        <td>
+        	<input type="text" class="inputCaixa" name="placarB" id="placarB" placeholder="Nº" style="text-align: center;"  required>
+        </td>
+       <td><img src="./views/images/star.png" width="47" height="47" ></td>
+        <td>
+        	<input type="text" class="inputCaixa" name="placarA" id="placarA" placeholder="Nº" style="text-align: center;"  required>
+        </td>
+        <td>X</td>
+        <td>
+        	<input type="text" class="inputCaixa" name="placarB" id="placarB" placeholder="Nº" style="text-align: center;"  required>
+        </td>
+        <td><img src="./views/images/star.png" width="47" height="47" ></td>
+        <td>
+        	<input type="text" class="inputCaixa" name="placarA" id="placarA" placeholder="Nº" style="text-align: center;"  required>
+        </td>
+        <td>X</td>
+        <td>
+        	<input type="text" class="inputCaixa" name="placarB" id="placarB" placeholder="Nº" style="text-align: center;"  required>
+        </td>
+       <td><img src="./views/images/star.png" width="47" height="47" ></td>
+    	<td>
+        	<input type="text" class="inputCaixa" name="placarA" id="placarA" placeholder="Nº" style="text-align: center;"  required>
+        </td>
+        <td>X</td>
+        <td>
+        	<input type="text" class="inputCaixa" name="placarB" id="placarB" placeholder="Nº" style="text-align: center;"  required>
+        </td>
+        <td><img src="./views/images/star.png" width="47" height="47" ></td>
+        <td>
+        	<input type="text" class="inputCaixa" name="placarA" id="placarA" placeholder="Nº" style="text-align: center;"  required>
+        </td>
+        <td>X</td>
+        <td>
+        	<input type="text" class="inputCaixa" name="placarB" id="placarB" placeholder="Nº" style="text-align: center;"  required>
+        </td>
+    </tr>
+    <tr>
+    <td></td>
+    <td>1° Tempo(30)'</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>Fim do Jogo(60)'</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>1° Tempo Extra</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>2° Tempo Extra</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>Tiro de 7 Metros</td>
+    <td></td>
+    <td></td>
     </tr>
 </table>
 <table class="medidas" boder="0" style=" border-bottom: 1px solid #CCC; padding: 4px; margin-bottom: 10px;">
        <thead>
         <tr>
-        	<th id="a" class="td"></th>
+        	
         	<th id="b"><small>A</small></th>
         	<th id="c"  colspan="3"><small>Tempo Técnico - Equipe A</small></th>
             <td></td>
             <th id="d" colspan="3"><small>Tempo Técnico - Equipe B</small></th>
-        	<th id="e"></th>
+        	
             <th id="f"><small>B</small></th>
         </tr>
        </thead>
        <tfoot>
     	<tr>
-        	<th class="td" id="a1" hearders="a"><small>n° de 7 Metros</small></th>
-            <th class="fundo td" id="b1" hearders="b"></th>
+        	
+            <th><input type="text" class="inputCaixa" name="7MetrosA" id="7MetrosA" placeholder="Nº" style="text-align: center;"  required></th>
             <th class="fundo td" id="c1" hearders="c">I</th>
             <th class="fundo td" id="c2" hearders="c">II</th>
             <th class="fundo td" id="c3" hearders="c">III</th>
@@ -195,9 +294,12 @@ background-color: #CC0000;
             <th class="fundo td" id="d1" hearders="d">I</th>
             <th class="fundo td" id="d2" hearders="d">II</th>
             <th class="fundo td" id="d3" hearders="d">III</th>
-            <th class="td" id="e1" hearders="e"><small>n° de 7 Metros</small></th>
-            <th class="fundo td" id="f1" hearders="f"></th>
+            
+             <th><input type="text" class="inputCaixa" name="7MetrosB" id="7MetrosB" placeholder="Nº" style="text-align: center;"  required></th>
          </tr>
+         <tr><th class="td" id="e1" hearders="e"><small>n° de 7 Metros</small></th>
+         <th></th><th></th><th></th><th></th><th></th><th></th><th></th><th class="td" id="e1" hearders="e"><small>n° de 7 Metros</small></th>
+      	 </tr>	
        </tfoot>
 </table>
 <table class="medidas" boder=0   style=" border-bottom: 1px solid #CCC; padding: 4px; margin-bottom: 10px;">
